e4c7ce6ad8967855780b519b758b3d3acada7f23
FazaMauJagoNgoding <fazafahri270706@gmail.com>
2026-02-11 22:08:09 +0700

    basic dom

--- DOM/main.js
+++ DOM/main.js
@@ -1,17 +1,23 @@
-const body = document.body
-let n = 0
-const angka = document.createElement("p");
-angka.textContent = n;
-body.append(angka);
+let cartBtn = document.getElementById("cart");
+let priceElement = document.getElementById("price");
+let cartBtnMin = document.getElementById("minus");
 
-function nambah() {
-    n += 1
-    angka.textContent = n;
-    body.append(angka);
-}
 
-function ngurang() {
-    n -= 1
-    angka.textContent = n;
-    body.append(angka);
-}
+let total = 0;
+const hargaProduk = 30; // harga produk
+
+cartBtn.addEventListener("click", function () {
+    total += hargaProduk;
+    priceElement.textContent = total + "$";
+});
+
+cartBtnMin.addEventListener("click", function () {
+    if (total < 0) {
+        hargaProduk = 0;
+        total -= 0;
+        priceElement.textContent = total + "$"
+    } else {
+        total -= hargaProduk
+        priceElement.textContent = total + "$"
+    }
+})
